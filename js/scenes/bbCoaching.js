4e016613c78f1418fcdec982f5068ab06ecf77a4
Qicheng Li <ql2232@nyu.edu>
2023-05-10 21:10:23 -0400

    Added change of time and tactic by coach using croquet

--- js/scenes/bbCoaching.js
+++ js/scenes/bbCoaching.js
@@ -2,16 +2,83 @@ import * as global from "../global.js";
 import * as cg from "../render/core/cg.js";
 import {Gltf2Node} from "../render/nodes/gltf2.js";
 import {g2} from "../util/g2.js";
-import {buttonState, joyStickState, viewMatrix} from "../render/core/controllerInput.js";
-import {COLORS, MAX_TIME} from "./const.js";
-import {gltfRoot} from "../global.js";
-import {quat} from "../render/math/gl-matrix.js";
+import {buttonState, joyStickState} from "../render/core/controllerInput.js";
+import {COLORS, MAX_TIME, NUM_PLAYERS} from "./const.js";
+import * as croquet from "../util/croquetlib.js";
 
-let currTime = 0
 let HUDIsShown = false;      // press right[1] to show hud, press again to hide it
 let hudButtonLock = false;
-let currPlayerIndex = -1;
+window.currPlayerIndex = -1;
 let drawButtonLock = false;
+window.playerList = [];
+
+window.names = [];
+for (let i = 0; i < NUM_PLAYERS + 1; i++) window.names.push(false);
+let whoIndex = {};
+let rolesSetComplete = false;
+window.currTime = 0
+
+function collectPreset() {
+    let playerPresetList = [];
+    for (let i = 0; i < NUM_PLAYERS; i++) {
+        playerPresetList.push({
+            directions: window.playerList[i].directions,
+            positions: window.playerList[i].positions,
+            startTimeList: window.playerList[i].startTimeList,
+            endTimeList: window.playerList[i].endTimeList
+        });
+    }
+    window.preset = playerPresetList;
+}
+
+let loadPresets = (preset) => {
+    console.log("current window.playerList:", window.playerList)
+    for (let i = 0; i < NUM_PLAYERS; i++) {
+        window.playerList[i].directions = preset[i].directions;
+        window.playerList[i].positions = preset[i].positions;
+        window.playerList[i].startTimeList = preset[i].startTimeList;
+        window.playerList[i].endTimeList = preset[i].endTimeList;
+    }
+}
+
+export let updateModel = e => {
+    if (window.demobbCoachingState) { // use window.demo[your-demo-name]State to see if the demo is active. Only update the croquet interaction when the demo is active.
+        if (e.userID === 0) {
+            updateTime(e.currTime);
+            if (e.preset) {
+                console.log("Loading presets:", e.preset)
+                loadPresets(e.preset)
+            }
+        }
+        if (rolesSetComplete) {
+            return;
+        }
+        if (!(e.who in whoIndex)) {
+            for (let i = 0; i < NUM_PLAYERS + 1; i++) {
+                if (!window.names[i]) {
+                    window.names[i] = e.who;
+                    whoIndex[e.who] = i;
+                    console.log("Name: ", window.names[i])
+                    console.log("Setting role of ", e.who, "to:", i)
+                    if (window.name == e.who) {
+                        console.assert(window.userID === undefined);
+                        window.userID = i;
+                        console.log("Set window.userID to:", window.userID)
+                    }
+                    return;
+                }
+            }
+            console.error("Should never get here!!!")
+            return;
+        }
+        for (let i = 0; i < NUM_PLAYERS + 1; i++) {
+            if (!window.names[i]) return;
+        }
+        console.log("roles set complete!");
+        console.log(window.names);
+        rolesSetComplete = true;
+    }
+}
 
 let hudButtonHandler = () => {
     if (buttonState.right[1] && buttonState.right[1].pressed && !hudButtonLock) {
@@ -93,6 +160,14 @@ class Court {
 }
 
 
+function updateTime(newTime) {
+    console.log("updating time from", window.currTime, " to: ", newTime)
+    window.tacticBoard.timeButton[window.currTime].updateColor('#a0aaba');
+    window.currTime = newTime;
+    window.tacticBoard.timeButton[window.currTime].updateColor('#37373f');
+    window.tacticBoard.timeButtonValue = newTime;
+}
+
 export const init = async model => {
     model.setTable(false)
     model.setRoom(false)
@@ -100,15 +175,12 @@ export const init = async model => {
     let boardBase = model.add()
     let fieldMap = boardBase.add('cube').texture('../media/textures/field.png');
 
-    let playerList = []
 
-    const numPlayers = 5
-
-    for (let i = 0; i < numPlayers; i++) {
-        playerList.push(new Player("./media/gltf/Basketball_Player/Basketball_Player.gltf", i, initialPosList[i], COLORS[i]));
+    for (let i = 0; i < NUM_PLAYERS; i++) {
+        window.playerList.push(new Player("./media/gltf/Basketball_Player/Basketball_Player.gltf", i, initialPosList[i], COLORS[i]));
     }
 
-    let tacticBoard = boardBase.add('cube').texture(() => {
+    window.tacticBoard = boardBase.add('cube').texture(() => {
         g2.setColor('white');
         g2.fillRect(0, 0, 1, 1);
         g2.textHeight(.04)
@@ -125,82 +197,82 @@ export const init = async model => {
         g2.fillText('↑', .965, .805, 'center');
         g2.fillText('23s', .965, .78, 'center');
         g2.fillText('-- Time Frames --', .76, .79, 'center');
-        g2.drawWidgets(tacticBoard);
+        g2.drawWidgets(window.tacticBoard);
     });
 
-    tacticBoard.timeButton = [];                                                //array of size 24 used to store the time button widgets
-    tacticBoard.visible = false;
-    tacticBoard.ID = -1;
-    tacticBoard.view = "global"
-    tacticBoard.timeButtonValue = 0;
+    window.tacticBoard.timeButton = [];                                                //array of size 24 used to store the time button widgets
+    window.tacticBoard.visible = false;
+    window.tacticBoard.ID = -1;
+    window.tacticBoard.view = "global"
+    window.tacticBoard.timeButtonValue = 0;
 
     // add trackpad
-    g2.addTrackpad(tacticBoard, .25, .47, '#fad4d4', ' ', () => {
-    }, 1, playerList);
+    g2.addTrackpad(window.tacticBoard, .25, .47, '#fad4d4', ' ', () => {
+    }, 1, window.playerList);
 
     // add buttons for all players
-    for (let i = 0; i < numPlayers; i++) {
-        g2.addWidget(tacticBoard, 'button', .65, .12 + i * .14, COLORS[i], '#' + i, () => {
-            tacticBoard.visible = false
+    for (let i = 0; i < NUM_PLAYERS; i++) {
+        g2.addWidget(window.tacticBoard, 'button', .65, .12 + i * .14, COLORS[i], '#' + i, () => {
+            window.tacticBoard.visible = false
             playerBoard.visible = true
             boardBase._children = [playerBoard, fieldMap]
             playerBoard.ID = i;
-            currPlayerIndex = i;
+            window.currPlayerIndex = i;
             playerBoard.path = [];
             playerBoard.drawMode = false;
 
             updateTimeButtonInPlayerBoard();
         }, 0.9);
 
-        g2.addWidget(tacticBoard, 'button', .85, .12 + i * .14, COLORS[i], "View", () => {
-            if (tacticBoard.view == "global") {
-                // set translation
-                let viewTranslate;
-                if (window.vr) {
-                    viewTranslate = [0, 0, 1];
+        g2.addWidget(window.tacticBoard, 'button', .85, .12 + i * .14, COLORS[i], "View", () => {
+            console.log("clicking on view button ", i, "from player", window.userID - 1);
+            if (window.userID === 0 || window.userID - 1 === i) {
+                if (window.tacticBoard.view == "global") {
+                    // set translation
+                    let viewTranslate;
+                    if (window.vr) {
+                        viewTranslate = [0, 0, 1];
+                    } else {
+                        viewTranslate = [0, 0, 4];
+                    }
+                    let posPlayer = window.playerList[i].positions[window.currTime];
+                    let posScene = [posPlayer[0] * Court.width, posPlayer[2], -posPlayer[1] * Court.height]
+                    global.gltfRoot.translation = cg.add(viewTranslate, cg.scale(posScene, -1))
+
+                    // set rotation
+                    // let rotation = quat.create();
+                    // let angel = window.playerList[i].directions[window.currTime] - Math.PI / 2;
+                    // console.log("rotation angel:")
+                    // quat.rotateY(rotation, rotation, window.playerList[i].directions[window.currTime] - Math.PI / 2);
+                    // global.gltfRoot.rotation = rotation
+
+                    window.tacticBoard.view = "local";
                 } else {
-                    viewTranslate = [0, 0, 4];
+                    global.gltfRoot.translation = [0, 0, 0];
+                    global.gltfRoot.rotation = [0, 0, 0, 1];
+                    window.tacticBoard.view = "global";
                 }
-                let posPlayer = playerList[i].positions[currTime];
-                let posScene = [posPlayer[0] * Court.width, posPlayer[2], -posPlayer[1] * Court.height]
-                global.gltfRoot.translation = cg.add(viewTranslate, cg.scale(posScene, -1))
-
-                // set rotation
-                // let rotation = quat.create();
-                // let angel = playerList[i].directions[currTime] - Math.PI / 2;
-                // console.log("rotation angel:")
-                // quat.rotateY(rotation, rotation, playerList[i].directions[currTime] - Math.PI / 2);
-                // global.gltfRoot.rotation = rotation
-
-                tacticBoard.view = "local";
-            } else {
-                global.gltfRoot.translation = [0, 0, 0];
-                global.gltfRoot.rotation = [0, 0, 0, 1];
-                tacticBoard.view = "global";
             }
-
-
         }, .9);
     }
 
 // add time buttons for tactic board
     for (let i = 0; i < 24; i++) {
-        tacticBoard.timeButton.push(g2.addWidget(tacticBoard, 'button', .55 + i * .018, .84, '#a0aaba', " ", () => {
-            tacticBoard.timeButton[currTime].updateColor('#a0aaba');
-            currTime = i;
-            tacticBoard.timeButton[currTime].updateColor('#37373f');
-            tacticBoard.timeButtonValue = i;
+        window.tacticBoard.timeButton.push(g2.addWidget(window.tacticBoard, 'button', .55 + i * .018, .84, '#a0aaba', " ", () => {
+            if (window.userID === 0) {
+                updateTime(i);
+            }
         }, 0.36));
     }
 
-    tacticBoard.identity().scale(.9, .9, .0001).opacity(0);
+    window.tacticBoard.identity().scale(.9, .9, .0001).opacity(0);
     fieldMap.identity().move(-0.45, -0.045, 0.0002).scale(.70, .76, .0001).opacity(0.2);
 
 
     // update the color of each time button based on the player and time frame selected in the player board
     let updateTimeButtonInPlayerBoard = () => {
         let currBoard = boardBase._children[0];
-        let currPlayer = playerList[currPlayerIndex];
+        let currPlayer = window.playerList[window.currPlayerIndex];
         let startIndex = 0;
         let endIndex = 0;
 
@@ -216,8 +288,8 @@ export const init = async model => {
                 endIndex += 1;
             }
 
-            if (currPlayerIndex !== -1 && withinRange) {
-                currBoard.timeButton[j].updateColor(COLORS[currPlayerIndex]);
+            if (window.currPlayerIndex !== -1 && withinRange) {
+                currBoard.timeButton[j].updateColor(COLORS[window.currPlayerIndex]);
             } else {
                 currBoard.timeButton[j].updateColor('#a0aaba');
             }
@@ -226,8 +298,8 @@ export const init = async model => {
 
     // create player boards
     let playerBoard = boardBase.add('cube').texture(() => {
-        let i = currPlayerIndex
-        let currPlayer = playerList[i];
+        let i = window.currPlayerIndex
+        let currPlayer = window.playerList[i];
         g2.setColor('white');
         g2.fillRect(0, 0, 1, 1);
         g2.textHeight(.04)
@@ -278,54 +350,61 @@ export const init = async model => {
 // Add time buttons for the player i
     for (let k = 0; k < 24; k++) {
         playerBoard.timeButton.push(g2.addWidget(playerBoard, 'button', .51 + k * .018, .90, '#a0aaba', " ", () => {
-            currTime = k;
-            if (playerBoard.startEditingMovement) {
-                let currPlayer = playerList[playerBoard.ID];
-                let lastEnd = currPlayer.endTimeList.length > 0 ? currPlayer.endTimeList[currPlayer.endTimeList.length - 1] : 0;
-                if (playerBoard.timeStart === -1 && k >= lastEnd) {
-                    playerBoard.timeStart = k;
-                    playerBoard.timeEnd = -1;
-                    currPlayer.startTimeList.push(k);
-                } else if (playerBoard.timeEnd === -1 && k > playerBoard.timeStart) {
-                    playerBoard.timeEnd = k;
-                    currPlayer.endTimeList.push(k);
-                    playerBoard.timeStart = -1;
+            if (window.userID === 0) {
+                window.currTime = k;
+                if (playerBoard.startEditingMovement) {
+                    let currPlayer = window.playerList[playerBoard.ID];
+                    let lastEnd = currPlayer.endTimeList.length > 0 ? currPlayer.endTimeList[currPlayer.endTimeList.length - 1] : 0;
+                    if (playerBoard.timeStart === -1 && k >= lastEnd) {
+                        playerBoard.timeStart = k;
+                        playerBoard.timeEnd = -1;
+                        currPlayer.startTimeList.push(k);
+                    } else if (playerBoard.timeEnd === -1 && k > playerBoard.timeStart) {
+                        playerBoard.timeEnd = k;
+                        currPlayer.endTimeList.push(k);
+                        playerBoard.timeStart = -1;
 
-                    playerBoard.startEditingMovement = false;
+                        playerBoard.startEditingMovement = false;
+                    }
+                    updateTimeButtonInPlayerBoard()
                 }
-                updateTimeButtonInPlayerBoard()
             }
         }, 0.36));
     }
     g2.addWidget(playerBoard, 'button', .75, .2, '#0cdfe0', "RETURN", () => {
-        boardBase._children = [tacticBoard, fieldMap]
+        boardBase._children = [window.tacticBoard, fieldMap]
         playerBoard.visible = false;
-        tacticBoard.visible = true;
+        window.tacticBoard.visible = true;
         playerBoard.drawMode = false;
         playerBoard.path = [];
+        collectPreset();
     }, 0.9);
 
     g2.addWidget(playerBoard, 'button', .6, .78, '#d965bb', "ADD", () => {
-        playerBoard.startEditingMovement = true
-        playerBoard.drawMode = false;
-        playerBoard.path = [];
+        if (window.userID === 0) {
+            playerBoard.startEditingMovement = true
+            playerBoard.drawMode = false;
+            playerBoard.path = [];
+        }
     }, 0.6)
 
-// Add delete button to delete the last interval
+    // Add delete button to delete the last interval
     g2.addWidget(playerBoard, 'button', .8, .78, '#7064e0', "DELETE", () => {
-        let currPlayer = playerList[playerBoard.ID];
-        if (currPlayer.startTimeList.length === currPlayer.endTimeList.length && currPlayer.startTimeList.length >= 0) {
-            currPlayer.startTimeList.pop();
-            currPlayer.endTimeList.pop();
+        if (window.userID === 0) {
+            let currPlayer = window.playerList[playerBoard.ID];
+            if (currPlayer.startTimeList.length === currPlayer.endTimeList.length && currPlayer.startTimeList.length >= 0) {
+                currPlayer.startTimeList.pop();
+                currPlayer.endTimeList.pop();
 
-            if (currPlayer.endTimeList.length > 0) {
-                let last_end = currPlayer.endTimeList[currPlayer.endTimeList.length - 1];
-                currPlayer.setAllFromEnd(last_end, true);
-                playerBoard.timeEnd = last_end;
-            } else {
-                currPlayer.resetPosAndDirect();
+                if (currPlayer.endTimeList.length > 0) {
+                    let last_end = currPlayer.endTimeList[currPlayer.endTimeList.length - 1];
+                    currPlayer.setAllFromEnd(last_end, true);
+                    playerBoard.timeEnd = last_end;
+                } else {
+                    currPlayer.resetPosAndDirect();
+                }
+                updateTimeButtonInPlayerBoard();
             }
-            updateTimeButtonInPlayerBoard();
         }
     }, 0.6)
 
@@ -337,10 +416,10 @@ export const init = async model => {
         }, 0.6));
     }
     g2.addTrackpad(playerBoard, .25, .47, '#fad4d4', ' ', () => {
-    }, 1, playerList);
+    }, 1, window.playerList);
 
-    let isDrawingMode = () => playerList[playerBoard.ID].endTimeList.length > 0
-        && playerList[playerBoard.ID].endTimeList.length === playerList[playerBoard.ID].startTimeList.length
+    let isDrawingMode = () => window.playerList[playerBoard.ID].endTimeList.length > 0
+        && window.playerList[playerBoard.ID].endTimeList.length === window.playerList[playerBoard.ID].startTimeList.length
 
 // handle the on/off of drawMode. Same logic as HudButtonHandler().
     let drawButtonHandler = () => {
@@ -359,11 +438,11 @@ export const init = async model => {
     }
 
     let changeDirection = () => {
-        if (currTime >= 0) {
-            console.assert(currPlayerIndex >= 0)
-            playerList[currPlayerIndex].directions[currTime] += 2 * model.deltaTime * joyStickState.right.x;
+        if (window.currTime >= 0) {
+            console.assert(window.currPlayerIndex >= 0)
+            window.playerList[window.currPlayerIndex].directions[window.currTime] += 2 * model.deltaTime * joyStickState.right.x;
             if (joyStickState.right.x)
-                console.log("directions:", playerList[currPlayerIndex].directions)
+                console.log("directions:", window.playerList[window.currPlayerIndex].directions)
         }
     }
 
@@ -375,16 +454,11 @@ export const init = async model => {
         return xyz;
     }
 
+    croquet.register('croquetDemo_1.25');
 
     model.animate(() => {
         boardBase.identity().boardHud().scale(1.3);
 
-
-        // console.log("viewMatrix")
-        // console.log(getMatrixXYZ(viewMatrix[0]))
-        // console.log(viewMatrix)
-        // console.log("global.gltfRoot.rotation")
-        // console.log(global.gltfRoot.rotation)
         hudButtonHandler();
         if (playerBoard.visible) {
             // current board is player board
@@ -394,19 +468,18 @@ export const init = async model => {
 
         if (HUDIsShown) {
             if (boardBase._children.length === 0) {
-                boardBase._children = [tacticBoard, fieldMap]
-                tacticBoard.visible = true;
+                boardBase._children = [window.tacticBoard, fieldMap]
+                window.tacticBoard.visible = true;
             }
         } else {
             if (boardBase._children.length > 0) {
                 boardBase._children[0].visible = false;
                 boardBase._children = [];
-                tacticBoard.visible = false;
+                window.tacticBoard.visible = false;
             }
-
         }
-        for (let i = 0; i < numPlayers; i++) {
-            playerList[i].update(currTime)
+        for (let i = 0; i < NUM_PLAYERS; i++) {
+            window.playerList[i].update(window.currTime)
         }
     });
 }
--- js/scenes/const.js
+++ js/scenes/const.js
@@ -1,2 +1,3 @@
-export let COLORS = ['#32a852', '#3e32a8', '#eaf04d', '#e36e14', '#f50fb7'];
-export let MAX_TIME = 24;
+export const COLORS = ['#32a852', '#3e32a8', '#eaf04d', '#e36e14', '#f50fb7'];
+export const MAX_TIME = 24;
+export const NUM_PLAYERS = 5;
